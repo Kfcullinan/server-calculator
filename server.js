b77a3f9c4c28a401fff2964ce114463d51a625e5
Keith Cullinan <kfcullinan@gmail.com>
2022-07-29 15:55:31 -0500

    added express

---
+++ server.js
@@ -0,0 +1,18 @@
+//create server app
+const express = require('express');
+const app = express();
+const port = 5000;
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log( 'running on port', PORT )
+})
